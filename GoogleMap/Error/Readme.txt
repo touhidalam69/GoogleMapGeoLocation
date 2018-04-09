2d2365be7be5b83252f9dee581355b8216295fd9
Md. Touhid Alam <14317784+touhidalam69@users.noreply.github.com>
2018-04-09 22:54:15 +0600

    This is first Commit
    Added html, javascript, css and Error folder

---
+++ GoogleMap/Error/Readme.txt
@@ -0,0 +1,10 @@
+We are using free api, So that some times it occurs this Error
+
+
+#########################################################################################################
+#													#
+#					Oops! Something went wrong					#
+#													#
+#	This page didn't load Google Maps correctly. See the JavaScript console for technical details	#
+#													#
+#########################################################################################################
---
+++ GoogleMap/src/app.js
@@ -0,0 +1,34 @@
+﻿var image = {
+    url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    size: new google.maps.Size(20, 32),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(0, 32)
+};
+
+function loadLocate() {
+    navigator.geolocation.getCurrentPosition(initialize, fail);
+}
+
+function initialize(position) {
+    var myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var mapOptions = {
+        zoom: 7,
+        center: myLatLng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    var map = new google.maps.Map(document.getElementById('map_canvas'),
+                                  mapOptions);
+
+    var userMarker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hi, I am here ...',
+        draggable: true,
+        icon: image
+    });
+}
+
+function fail() {
+    alert('navigator.geolocation failed, may not be supported');
+}
